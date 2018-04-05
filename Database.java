88a03e8c6a73f88962ce67592725fd1120d6974c
SDas-Student <35471725+SDas-Student@users.noreply.github.com>
2018-04-05 19:37:59 -0400

    Update Database.java

--- Database.java
+++ Database.java
@@ -2,9 +2,16 @@ import java.sql.*;
 
 public class Database {
 	 public static void main( String args[] ) {
-	      Connection c = null;
+		 String url = "jdbc:sqlite:test.db";
+		 String username = "student";
+		 String password = "student";
+		 
+	     
 	      
 	      try {
+	    	  //Get connection with Database
+	    	  Connection c = DriverManager.getConnection(url, username, password);
+	    	  
 	         Class.forName("org.sqlite.JDBC");
 	         c = DriverManager.getConnection("jdbc:sqlite:test.db");
 	      } catch ( Exception e ) {
@@ -14,4 +21,5 @@ public class Database {
 	      System.out.println("Opened database successfully");
 	   }
 }
+
 
