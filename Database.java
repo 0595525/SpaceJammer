3789a8c0c13a6824e48ed350c4346a279201ab82
SDas-Student <35471725+SDas-Student@users.noreply.github.com>
2018-04-05 20:29:23 -0400

    Update Database.java

--- Database.java
+++ Database.java
@@ -11,9 +11,19 @@ public class Database {
 	      try {
 	    	  //Get connection with Database
 	    	  Connection c = DriverManager.getConnection(url, username, password);
+	  // Connection conn = DriverManager.getConnection("jdbc:sqlite:D:\\databases\\testjava.db");
+	    	  //Create a statement
+	    	  Statement myScore = c.createStatement();
+	    	  
+	    	  //Execute sql query
+	   /* 	  Class<?> sql = 
 	    	  
 	         Class.forName("org.sqlite.JDBC");
-	         c = DriverManager.getConnection("jdbc:sqlite:test.db");
+	         c = DriverManager.getConnection("jdbc:sqlite:test.db");  */
+	    	  
+	    	  myScore.execute("CREATE TABLE IF NOT EXISTS scoretable (score INTEGER)");
+	    	  myScore.execute("INSERT INTO scoretable (score) VALUES ('{0}')");
+	    	  
 	      } catch ( Exception e ) {
 	         System.err.println( e.getClass().getName() + ": " + e.getMessage() );
 	         System.exit(0);
