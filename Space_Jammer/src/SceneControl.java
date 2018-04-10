d71981e8b202065d3f9ef83268f1d15855d90c9d
jiaxuan lu <jiaxuan12345@hotmail.com>
2018-04-09 20:09:01 -0400

    Update SceneControl.java

--- Space_Jammer/src/SceneControl.java
+++ Space_Jammer/src/SceneControl.java
@@ -1,26 +1,30 @@
+
+//Reference:
+//https://github.com/daniel-chai/javafx-game/tree/master/src
+//https://github.com/AlmasB/FXTutorials/tree/master/src/com/almasb
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.*;
 
-
+/* this is the class that will control all the scene, there are menu scene, start scene, and game over scene. 
+ * only one stage is using during the gmae this is the reason that the primaryStage is a parameter in the constructor
+ */
 public class SceneControl {
 	private Stage stage;
 	private Timeline animation;
+
 	//private 
 	public static int Msecond = 1000 / 60;
 	public static double second = 1 / 60;
 	
+	////Constructor for the sceneControl 
 	public SceneControl (Stage primaryStage) {
 		this.stage = primaryStage;
 		this.animation = new Timeline();
 		stage.show();	
 	}
-	
-	
 	
 /** **/
 		
@@ -32,25 +36,29 @@ public class SceneControl {
 		stage.setScene(menuScene);
 	}
 	
+	
 	// to the Startscene
-	public void startFight (SceneControl scene ) {
+	public void startFight (SceneControl scene) {
 		Start fight = new Start(scene);
 		Scene FightScene = fight.init(Main.SIZE, Main.SIZE);
+		
 		stage.setScene(FightScene);
-		KeyFrame frame = new KeyFrame(Duration.millis(Msecond), e-> fight.point(second));	
-		setLoop(frame);
+		KeyFrame frame = new KeyFrame(Duration.millis(Msecond), e-> fight.point(second));	//Eventhandle
+		GameLoop(frame);
 	}
-	//done
+	
+	
+	//the game is over and player lose
 	public void GameOver(SceneControl scene) {
 		animation.stop();
 		Lost lost = new Lost(scene);
 		Scene lostScene = lost.init(Main.SIZE, Main.SIZE);
 		stage.setScene(lostScene);
 		
+		
 	}
 	
-	
-	private void setLoop(KeyFrame frame) {
+	private void GameLoop(KeyFrame frame) {
 		animation = new Timeline();
 		animation.setCycleCount(Timeline.INDEFINITE);
 		animation.getKeyFrames().add(frame);
