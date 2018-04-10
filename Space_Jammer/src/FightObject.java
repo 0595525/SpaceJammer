27acc71010c45f8d6970b9569e3f48880c63e4f0
jiaxuan lu <jiaxuan12345@hotmail.com>
2018-04-09 20:02:09 -0400

    Update FightObject.java

--- Space_Jammer/src/FightObject.java
+++ Space_Jammer/src/FightObject.java
@@ -1,10 +1,19 @@
+//Reference:
+//https://github.com/daniel-chai/javafx-game/tree/master/src
+//https://github.com/AlmasB/FXTutorials/tree/master/src/com/almasb
+
 import java.util.HashSet;
 import java.util.Set;
-
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+
+/*
+ * this is the abstract class. it is the super class for the Start class.
+ */
+
 
 public abstract class FightObject {
 	
@@ -12,35 +21,54 @@ public abstract class FightObject {
 	protected Hero heroObject;
 	protected Rectangle hero;
 	protected Set<Bullet> BulletObjects;
-	protected long counter = 2L;
+	protected long counter = 0L;
 	protected Group heroBullet;
 	protected Group root;
-	public int count = 0;
+	protected int Score = 0;
+	private Text BottonText;
+	
+	protected void Restart() {
+		scene.MenuScene(scene);
+	}
 	
 	//game over
 	protected void lost() {
 		scene.GameOver(scene);
 	}
+		
+	// create text at botton for the score 
+	protected void AddText () {
+	    BottonText = Object.createT("Score: 0", 0, Main.SIZE);
+		BottonText.setFill(Color.RED);	
+		root.getChildren().add(BottonText);	
+	}
+	//updateing the score
+	protected void UppdateScore(String text, int score) {
+		root.getChildren().remove(BottonText);
+		BottonText = Object.createT(text + score, 0, Main.SIZE);
+		BottonText.setFill(Color.RED);
+		root.getChildren().add(BottonText);	
+	}
 	
+	// aad the hero recatangle
 	protected void addhero() {
 		heroObject = new Hero();
-		hero = heroObject.getHero();
+		hero = heroObject.GetHero();
 		root.getChildren().add(hero);
 	}
-	
+	//create the bullet 
 	protected void addheroBullet() {
 		BulletObjects = new HashSet<Bullet>();
+		
 	}
-	
+	// update the bullet
 	protected void updateBullet() {
 		root.getChildren().remove(heroBullet);
 		heroBullet = new Group();
 		transferBullet(BulletObjects, heroBullet);
 		
-		
-		
 	}
-	
+	//For each bullet in the Set, add the corresponding Rectangle to the Group
 	protected void transferBullet(Set<Bullet> bulletSet, Group bullet) {
 		// TODO Auto-generated method stub
 		for (Bullet BulletObject : bulletSet) {
@@ -48,8 +76,9 @@ public abstract class FightObject {
 		}
 		root.getChildren().add(bullet);
 	}
-
-	protected void moveBullet() {
+	
+	//move the bullet in the to corret dirction
+	protected void MoveBullet() {
 		for (Bullet BulletObject : BulletObjects) {
 			BulletObject.move();
 		}
@@ -57,13 +86,21 @@ public abstract class FightObject {
 	
 	}
 	
+	//shoot the bullet in up, right and left direction.
+	protected void shoot(String direction) {
+		Bullet BulletObject = new Bullet(Hero.bulletSize, Color.GOLD, direction, hero);
+		BulletObjects.add(BulletObject);
+		updateBullet();
+		
+	}
+	// to check if the bullet hit the enemy recatangle
 	protected boolean HitEnemy ( Set<Bullet> bulletSet, Group bullets, Rectangle r) {
 		for (Bullet BulletObject : bulletSet) {
 			Rectangle bullet = BulletObject.getBullet();
 			if (bullet.getBoundsInLocal().intersects(r.getBoundsInLocal())) {
 				bulletSet.remove(BulletObject);
 				bullets.getChildren().remove(bullet);
-				count++;
+				//Score = Score+1;
 				return true;
 			}
 		}
@@ -72,17 +109,28 @@ public abstract class FightObject {
 		
 	}
 	
-	//protected void check
-	protected void EnemyHitHero(Rectangle enemy) {
-		if (hero.getBoundsInLocal().intersects(enemy.getBoundsInLocal())) {
-			lost();
-			count--;
-		}
-	}
-	protected void shoot(String direction) {
-		Bullet BulletObject = new Bullet(Hero.bulletSize, Color.RED, direction, hero);
-		BulletObjects.add(BulletObject);
-		updateBullet();
+	// when the hero bullet hit the enemy recatangle, it will remove the enemy recatangle 
+	protected void removeEnemy (Group group, boolean remove) {
+		for(Node node : group.getChildren()) {
+			Rectangle r = (Rectangle) node;
+			if (HitEnemy(BulletObjects,heroBullet,r)) {
+				Score = Score+1;
+
+				if (remove) {
+					group.getChildren().remove(r);
+				}
+				return;
+			}
+		}	
 		
 	}
+	
+	//  if the enemy recatangle hit hero, lose the game 
+	protected void EnemyHitHero(Rectangle enemy) {
+		if (hero.getBoundsInLocal().intersects(enemy.getBoundsInLocal())) {
+			lost();//i mean lose the game 
+		}
+	}
+	
+	
 }
