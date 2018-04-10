8504bdf20f8cef3b6ccfb49ed7147d21f7917014
jiaxuan lu <jiaxuan12345@hotmail.com>
2018-04-09 20:06:45 -0400

    Update Lose.java

--- Lose.java
+++ Lose.java
@@ -1,3 +1,7 @@
+//Reference:
+//https://github.com/daniel-chai/javafx-game/tree/master/src
+//https://github.com/AlmasB/FXTutorials/tree/master/src/com/almasb
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -5,41 +9,49 @@ import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
-public class Lost implements SceneInterface{
+
+// i mean lose  not lost (lol)
+public class Lost extends FightObject implements IScene{
 
 	private SceneControl scene;
-	private Scene lost;
+	private Scene lostScene;
 	private Group root;
 
+	
+	////Constructor for the lost
 	public Lost (SceneControl scene) {
 		this.scene = scene;
-		 
+		
 	}
 
 	@Override
 	public Scene init(int width, int height) {
 		// TODO Auto-generated method stub
 		root = new Group();
-		lost = new Scene(root, width, height, Color.RED);
-		//addLostText();
-		//addStartButton();
-		return lost;
+		lostScene = new Scene(root, width, height, Color.LAVENDER);
+		addLostText();
+		addStartButton();
+		return lostScene;
 	}
-	/*
+
+	//add  lose text message
 	public void addLostText() {
-		Text lostText = Object.createT("Game Over !! " , 60, 60);
+		Text lostText = Object.createT("Game Over !! " , 60, 60);	
 		root.getChildren().add(lostText);
 	}
+	
+	//add start button
 	public void addStartButton() {
-		Button startButton = Object.createB("Again", 60, 100);
-		startButton.setOnAction(new EventHandler<ActingEVENT>(){
+		Button startButton = Object.CreateB("Again", 300, 300);
+		startButton.setOnAction(new EventHandler<ActionEvent>(){
+			
 			@Override
 			public void handle(ActionEvent event) {
-				SceneControl.ToStartScene (scene);
+				scene.MenuScene (scene);
 			}
 			
 		});
-		root.getChildren().add(againButton)
+		root.getChildren().add(startButton);
 	}
-	*/
+	
 }
