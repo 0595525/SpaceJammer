f0f9f3ce77d04416e5195adc12a84928eed92118
jiaxuan lu <jiaxuan12345@hotmail.com>
2018-04-09 20:05:48 -0400

    Update IScene.java

--- Space_Jammer/src/IScene.java
+++ Space_Jammer/src/IScene.java
@@ -1,5 +1,9 @@
-
+//Reference:
+//https://github.com/daniel-chai/javafx-game/tree/master/src
+//https://github.com/AlmasB/FXTutorials/tree/master/src/com/almasb
 import javafx.scene.Scene;
+
+// it is the interface for create the scene. 
 
 public interface IScene {
 	Scene init(int width, int height);
