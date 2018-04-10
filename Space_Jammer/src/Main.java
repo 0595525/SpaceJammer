9dfc88690c13346751c84bd2f14ff827a06568cd
jiaxuan lu <jiaxuan12345@hotmail.com>
2018-04-09 20:07:36 -0400

    Update Main.java

--- Space_Jammer/src/Main.java
+++ Space_Jammer/src/Main.java
@@ -1,40 +1,32 @@
-import javafx.animation.Timeline;
+//Reference:
+//https://github.com/daniel-chai/javafx-game/tree/master/src
+//https://github.com/AlmasB/FXTutorials/tree/master/src/com/almasb
 import javafx.application.Application;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.text.Font;
+
+
 import javafx.stage.Stage;
 
 public class Main extends Application  {
 
 	public static int SIZE = 800;
-	private String score;
-	//private Label lblScore = new Label("Score: " + score);
+
 
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 		
-		
+        
 		primaryStage.setTitle("Space Jammer");
-    		SceneControl scene = new SceneControl(primaryStage);
-    		scene.MenuScene(scene);
-    		
-    		
-    		/*
-    		final TextField TF = new TextField();
-    		TF.setPromptText("Enter Play Name");
-    		TF.setPrefColumnCount(20);
-    		TF.getText();
-    		GridPane.setConstraints(TF,0,0);
-    		*/
+		primaryStage.setResizable(false);
 		
+		SceneControl scene = new SceneControl(primaryStage);
+    		scene.MenuScene(scene);
+    	  
+    		
 		
 	}
+	
 	public static void main (String[] args) {
 		launch (args);
 	}
